fbdd284ac64a661da31c2e093c6170df564b7784
Angela Lopez Z <74939915+glopzel@users.noreply.github.com>
2022-06-10 00:35:10 -0400

    deleted console.log

--- main.js
+++ main.js
@@ -2,14 +2,13 @@ document.querySelector('button').addEventListener('click', getFetch)
 
 function getFetch(){
   const choice = document.querySelector('input').value
-  console.log(choice)
 
   const url = `https://api.nasa.gov/planetary/apod?api_key=zU71SV2z8UAS2tpSRxtx9Ii4giGUAk6QIufK4bCn&date=${choice}`
 
   fetch(url)
       .then(res => res.json()) // parse response as JSON
       .then(data => {
-        console.log(data)
+        
         if( data.media_type === 'image' ){
           document.querySelector('img').src = data.hdurl
         }else if(data.media_type === 'video'){
