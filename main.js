0d52ae3740874b7603e121e514ee1a1e098ed854
Angela Lopez Z <74939915+glopzel@users.noreply.github.com>
2022-06-12 22:40:03 -0400

    update with async

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 document.querySelector('button').addEventListener('click', getFetch)
 
-function getFetch(){
+async function getFetch(){
   const choice = document.querySelector('input').value
 
   const res = await fetch(`https://api.nasa.gov/planetary/apod?api_key=zU71SV2z8UAS2tpSRxtx9Ii4giGUAk6QIufK4bCn&date=${choice}`);
